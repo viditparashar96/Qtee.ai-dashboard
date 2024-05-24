8225ff0582640eaa71b79f0ebebd9095f91107bf
vidit <viditsharma818@gmail.com>
2024-05-24 20:06:25 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Benefits from "@/components/Benefits";
+import Collaboration from "@/components/Collaboration";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Roadmap from "@/components/Roadmap";
+import Services from "@/components/Services";
+
+import ButtonGradient from "@/assets/svg/ButtonGradient";
+export default function Home() {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+}
